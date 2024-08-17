1e20b873c19299508a3104278c51347905b70094
dinhthituyetchinh <fengdinh@gmail.com>
2024-08-17 07:58:47 +0700

    init project

---
+++ XuongMayBE/XuongMayBE/Data/GarmentFactoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace XuongMayBE.Data
+{
+    public class GarmentFactoryContext : DbContext
+    {
+        // Thư mục Data chỉ chứa Entity và Context
+        //Class này tương ứng với DB
+        public GarmentFactoryContext(DbContextOptions<GarmentFactoryContext> option) : base(option)
+        {
+
+        }
+    }
+    #region Dbset
+
+    #endregion
+}
