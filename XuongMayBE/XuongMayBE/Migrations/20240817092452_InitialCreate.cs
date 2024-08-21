fe0e92104c0ad20a15065473765c59282e0c9192
dinhthituyetchinh <fengdinh@gmail.com>
2024-08-21 14:54:14 +0700

    TuyetChinh UPDATE

--- XuongMayBE/XuongMayBE/Migrations/20240817092452_InitialCreate.cs
+++ XuongMayBE/XuongMayBE/Migrations/20240817092452_InitialCreate.cs
@@ -9,21 +9,6 @@ namespace XuongMayBE.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Product",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Price = table.Column<double>(type: "float", nullable: false),
-                    Quantity = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Product", x => x.Id);
-                });
-            migrationBuilder.CreateTable(
                name: "ProductionLines",
                columns: table => new
                {
